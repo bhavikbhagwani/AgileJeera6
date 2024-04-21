93e5045ba37ae657b0aca04d85f97d268b210f24
bereket tesfaye <berekettesfaye7909@gmail.com>
2024-04-21 19:53:29 +0200

    modified authntication

--- login_page.py
+++ login_page.py
@@ -1,68 +1,87 @@
+import pyrebase
 from tkinter import *
+from write_to_data_base import save_to_data_base
 
-# Main window 
-Window = Tk()
-Window.title("Aumeter")
+class Login_page:
+    def __init__(self):
+        self.saver = save_to_data_base()
+        self.master = Tk()
+        self.master.title("Aumeter")
 
-# Get the screen width and height
-screen_width = Window.winfo_screenwidth()
-screen_height = Window.winfo_screenheight()
+        # Get the screen width and height
+        screen_width = self.master.winfo_screenwidth()
+        screen_height = self.master.winfo_screenheight()
 
-# Set the geometry of the window to fill the entire screen
-Window.geometry(f"{screen_width}x{screen_height}+0+0")
+        # Set the geometry of the window to fill the entire screen
+        self.master.geometry(f"{screen_width}x{screen_height}+0+0")
 
-#background for the window
-Window.config(background='#D8A9B3')
+        # Background for the window
+        self.master.config(background='#D8A9B3')
 
-title_label = Label(Window, text='Aumeter', font = ("Algerian", 60),
-                     padx= 20, pady= 20,
-                     bg='#D8A9B3')
-title_label.pack()
+        self.title_label = Label(self.master, text='Aumeter', font=("Algerian", 60),
+                                 padx=20, pady=20,
+                                 bg='#D8A9B3')
+        self.title_label.pack()
 
-#icon
-icon = PhotoImage(file='icon.png')
-Window.iconphoto(True, icon)
+        # Icon
+        icon = PhotoImage(file='icon.png')
+        self.master.iconphoto(True, icon)
 
-# StringVar objects to store the username and password
-username_var = StringVar()
-password_var = StringVar()
+        # StringVar objects to store the username and password
+        self.username_var = StringVar()
+        self.password_var = StringVar()
+
+        # Username label
+        self.username_label = Label(self.master, text="Username: ", font=("Forte", 25), bg='#D8A9B3', fg='Black')
+        self.username_label.place(x=5, y=240)
+
+        # Username entry field
+        self.username_entry = Entry(self.master, textvariable=self.username_var, bd=2, relief='ridge')
+        self.username_entry.place(x=100, y=300, width=280)
+
+        # Password label
+        self.password_label = Label(self.master, text='Password: ', font=("Forte", 25), bg='#D8A9B3')
+        self.password_label.place(x=5, y=360, anchor='w')
+
+        # Password entry field
+        self.password_entry = Entry(self.master, textvariable=self.password_var, show='*', bd=2, relief='ridge')
+        self.password_entry.place(x=100, y=400, width=280)
+
+        # Another label
+        additional_image = PhotoImage(file='image1.png')
+
+        # Reduce image size
+        new_width, new_height = 400, 400  # Adjust the size as needed
+        additional_image = additional_image.subsample(int(additional_image.width() / new_width),
+                                                      int(additional_image.height() / new_height))
+
+        # Additional label with resized image
+        self.additional_label = Label(self.master, image=additional_image, bg='#D8A9B3', relief='raised')
+        self.additional_label.place(x=screen_width - new_width + 360, y=150, anchor='ne')
+
+        # Meditation word
+        self.meditation_text_label = Label(self.master, text="Inhale the future, exhale the past.",
+                                           font=("Forte", 18), wraplength=300, justify=LEFT, bg='#D8A9B3')
+        self.meditation_text_label.place(x=900, y=580)
+
+        self.login_button = Button(self.master, text="Login", font=("Forte", 20), bg='white', fg='black', command=self.log_in)
+        self.login_button.place(x=110, y=460, width=120)
+
+        self.signup_button = Button(self.master, text="Sign Up", font=("Forte", 20), bg='white', fg='black', command=self.sign_up)
+        self.signup_button.place(x=250, y=460, width=120)
+    
+        self.master.mainloop()
+    
+    def log_in(self):
+        self.saver.log_in(str(self.username_entry.get()),str(self.password_entry.get()))
+        
+    
+    def sign_up(self):
+        self.saver.sign_up(str(self.username_entry.get()),str(self.password_entry.get()))
+        
 
 
-# Username label 
-username_label = Label(Window, text="Username: ", font=("Forte", 25), bg='#D8A9B3', fg='Black')
-username_label.place(x=5, y=240 )
-
-# Username entry field
-username_entry = Entry(Window, textvariable=username_var ,bd=2, relief= 'ridge')
-username_entry.place(x=100, y=300 , width= 280)
-
-# Password label
-password_label = Label(Window, text='Password: ', font=("Forte", 25), bg='#D8A9B3')
-password_label.place(x=5, y=360, anchor='w')
-
-# Password entry field
-password_entry = Entry(Window, textvariable=password_var,show='*', bd = 2, relief='ridge') 
-password_entry.place(x = 100, y = 400, width=280)
-
-
-#Another lable
-additional_image = PhotoImage(file='image1.png')
-
-# Reduce image size
-new_width, new_height = 400, 400  # Adjust the size as needed
-additional_image = additional_image.subsample(int(additional_image.width() / new_width), int(additional_image.height() / new_height))
-
-# Additional label with resized image
-additional_label = Label(Window, image=additional_image,bg='#D8A9B3', relief='raised')
-additional_label.place(x=screen_width - new_width + 360, y=150, anchor='ne')
-
-#medittiation word
-meditation_text_label = Label(Window, text= "Inhale the future, exhale the past." , 
-                              font=("Forte", 18), wraplength=300, justify=LEFT, bg='#D8A9B3')
-meditation_text_label.place(x= 900, y=580)
-
-
-login_button = Button(Window, text="Login", font=("Forte", 20), bg='white', fg='black', )
-login_button.place(x=170, y=480, width=120)
-
-Window.mainloop()
+if __name__ == "__main__":
+   
+    my_app = Login_page()
+    
---
+++ write_to_data_base.py
@@ -0,0 +1,45 @@
+import pyrebase
+from tkinter import *
+from tkinter import messagebox
+
+class save_to_data_base:
+    def __init__(self):
+
+        self.firebaseConfig = {
+        "apiKey" : "AIzaSyDqp5UMRewvQ7AqBK3hUJ-WC35VgiY6IKo",
+        "authDomain" : "aumeter-39cb1.firebaseapp.com",
+        "projectId" : "aumeter-39cb1",
+        "storageBucket" : "aumeter-39cb1.appspot.com",
+        "databaseURL" : "https://aumeter-39cb1-default-rtdb.firebaseio.com/",
+        "messagingSenderId" : "738275596786",
+        "appId" : "1:738275596786:web:5b35e22bd376bef035736b",
+        "measurementId" : "G-BHL5KS350N"
+        }
+        self.firebase = pyrebase.initialize_app(self.firebaseConfig)
+        self.database = self.firebase.database()
+        self.authentication = self.firebase.auth()
+        
+      
+        
+    
+    def log_in(self,email,password):
+        try:
+            self.authentication.sign_in_with_email_and_password(email,password)
+            messagebox.showinfo("Success","Log In Successful, you will be directed to the home page")
+        except:
+            messagebox.showerror("Error","Log in not successful, wrong email or password")
+        return
+    
+    def sign_up(self,email,password):
+        try:
+            self.authentication.create_user_with_email_and_password(email,password)
+            messagebox.showinfo("Success","Sign Up Successful, you will be directed to the home page")
+        except:
+            messagebox.showerror("Error","Sign up not successful, wrong email or password")
+        return
+
+
+
+
+
+
