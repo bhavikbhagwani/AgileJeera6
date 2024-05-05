280adf651c0bdacc634b359c172d53008850e03b
Sham Saravaiahgari <sham.saravai@gmail.com>
2024-05-05 19:02:13 +0200

    create widget funtion

--- OOP_Login_Window.py
+++ OOP_Login_Window.py
@@ -10,4 +10,20 @@ class LoginWindow:
         self.root.config(background='#D8A9B3')
         self.create_widgets()
 
-    
+    def create_widgets(self):
+        self.title_label = Label(self.root, text='Aumeter', font=("Algerian", 60),
+                            padx=20, pady=20, bg='#D8A9B3')
+        self.title_label.pack()
+
+        self.login_body = Frame(self.root, width=400, height=500, bg="#D8A9B3", highlightthickness=2, highlightbackground="white")
+        self.login_body.place(x=50, y=100)
+
+        self.username_label = Label(self.login_body, text="Email: ", font=("Forte", 25), bg='#D8A9B3', fg='Black')
+        self.username_label.place(x=5, y=100)
+
+        self.username_entry = Entry(self.login_body, bd=2, relief='ridge')
+        self.username_entry.place(x=100, y=160, width=280, height=25)
+
+        self.password_label = Label(self.login_body, text='Password: ', font=("Forte", 25), bg='#D8A9B3')
+        self.password_label.place(x=5, y=220, anchor='w')
+#    
