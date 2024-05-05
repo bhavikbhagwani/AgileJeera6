ed012364f4370dbe9027216b6170c729084ff05b
Sham Saravaiahgari <sham.saravai@gmail.com>
2024-05-05 18:55:10 +0200

    added widget function

--- OOP_Favorites_Page.py
+++ OOP_Favorites_Page.py
@@ -13,4 +13,12 @@ class FavoritesWindow:
         self.root.geometry(f"{self.screen_width}x{self.screen_height}")
         self.create_widgets()
 
-    
+    def create_widgets(self):
+        self.header = Label(self.root, text="My Favorites", fg="black", bg="lightgrey", width=100, height=5, font=("Arial", 16))
+        self.header.pack(side="top", padx=10, pady=50)
+
+        self.favorites_body = Frame(self.root, width=700, height=500, bg="white", highlightthickness=2, highlightcolor="black")
+        self.favorites_body.place(x=250, y=200)
+
+        self.back_home_button = Button(self.root, text="Back to Home", width=20, height=3, bg="lightgrey", font=("Arial", 13, "bold"), command=self.show_home_page_from_favorites_page)
+        self.back_home_button.place(x=((self.screen_width - self.back_home_button.winfo_reqwidth()) // 2), y=625)
