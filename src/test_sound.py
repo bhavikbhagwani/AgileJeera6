e1c9515bf6ceff6ce67dfffa99db09c27e52af17
Your Name <ghazalchamto@gmail.com>
2024-05-15 23:20:53 +0200

    sount unit test updated

--- src/test_sound.py
+++ src/test_sound.py
@@ -1,6 +1,5 @@
 import unittest
-from unittest.mock import MagicMock, patch
-from sounds import Sounds  
+from sounds import Sounds
 
 class TestSounds(unittest.TestCase):
     def setUp(self):
@@ -18,31 +17,14 @@ class TestSounds(unittest.TestCase):
         # Test an invalid sound number
         self.assertIsNone(self.sounds.get_sound_name(100))
 
-    @patch('requests.get')
-    @patch('io.BytesIO')
-    @patch('pygame.mixer.init')
-    @patch('pygame.mixer.music.load')
-    @patch('pygame.mixer.music.play')
-    def test_play_sound(self, mock_play, mock_load, mock_init, mock_bytesio, mock_requests_get):
-        # Mock response object
-        mock_response = MagicMock()
-        mock_response.content = b'some_audio_data'
-        mock_requests_get.return_value = mock_response
-        # Calling method
-        self.sounds.play_sound(1)
-        # Assertions
-        mock_requests_get.assert_called_once_with(self.sounds.get_sound_url(1))
-        mock_bytesio.assert_called_once_with(b'some_audio_data')
-        mock_init.assert_called_once()
-        mock_load.assert_called_once_with(mock_bytesio())
-        mock_play.assert_called_once()
+    def test_play_sound(self):
+        # Check if play_sound() method doesn't raise any exceptions
+        self.sounds.play_sound(1)  # Playing a valid sound
+        self.sounds.play_sound(100)  # Playing an invalid sound
 
-    @patch('pygame.mixer.music.stop')
-    def test_stop_sound(self, mock_stop):
-        # Calling method
+    def test_stop_sound(self):
+        # Check if stop_sound() method doesn't raise any exceptions
         self.sounds.stop_sound()
-        # Assertion
-        mock_stop.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
